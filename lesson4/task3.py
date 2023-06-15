94a219cf89167701ecec3bc48bd9f2069ef50efb
Victor <zvic21@gmail.com>
2023-06-15 15:25:00 +0300

    15/06 2023

---
+++ lesson4/task3.py
@@ -0,0 +1,5 @@
+a, b = input().split()
+string1 = "Переменная a = "
+string2 = ", переменная b = "
+result = string1 + a + string2 + b
+print(result)
---
+++ lesson4/task4.py
@@ -0,0 +1,6 @@
+a = input()
+string1 = "Строка: "
+string2 = ". Длина: "
+b = str(len(a))
+result = string1 + a + string2 + b
+print(result)
